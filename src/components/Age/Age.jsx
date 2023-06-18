d2153425eba86db72a710d4614cfcb54ae80652b
Noa <noasalgadodev@gmail.com>
2023-06-18 18:10:08 +0200

    Add desktop styles

--- src/components/Age/Age.jsx
+++ src/components/Age/Age.jsx
@@ -5,14 +5,14 @@ function Age({ age }) {
   return (
     <div className='age'>
       <p>
-        <span>{days ? days : '--'}</span> {days !== 1 ? 'days' : 'day'}
+        <span>{years ? years : '--'}</span> {years !== 1 ? 'years' : 'year'}
       </p>
       <p>
         <span>{months || months === 0 ? months : '--'}</span>{' '}
         {months !== 1 ? 'months' : 'month'}
       </p>
       <p>
-        <span>{years ? years : '--'}</span> {years !== 1 ? 'years' : 'year'}
+        <span>{days ? days : '--'}</span> {days !== 1 ? 'days' : 'day'}
       </p>
     </div>
   );
